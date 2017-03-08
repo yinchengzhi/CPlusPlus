653f593f52b2a712efa0efc6bdf1ec619169181c
Valar Morghulis <524672142@qq.com>
2017-03-08 08:55:54 +0800

    update

---
+++ 7/strgfun.cpp
@@ -0,0 +1,38 @@
+
+
+#include<iostream>
+int c_in_str(const char *str,char ch);
+int main(){
+
+    using namespace std;
+    char mmm[15] = "minimum";                       //string in an array
+    //some system require preceding char with static to
+    //enable array initialization
+
+    char *wail = "ululate";                         //wail points to string
+
+    int ms = c_in_str(mmm,'m');
+    int us = c_in_str(wail,'u');
+
+    cout<<ms<<" m characters in "<<mmm<<endl;
+    cout<<us<<" u characters in "<<wail<<endl;
+
+    return 0;
+}
+
+// this function counts the number of ch characters
+// in the string str
+
+int c_in_str(const char *str, char ch){
+    int count = 0;
+
+    while(*str){                                    //quit when *str is '\0'
+        if(*str==ch)
+            count++;
+        str++;
+    }
+    return count;
+}
+
+
+
